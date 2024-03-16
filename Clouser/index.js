111b108cfc330d572c2ffd1d85f4bb7b5a3d9788
Mac <mac@Macs-MacBook-Pro.local>
2024-03-17 04:14:05 +0500

    clouser

--- Clouser/index.js
+++ Clouser/index.js
@@ -2,16 +2,45 @@
 // ise kehte hein koi child function apne parent unction ka variable use kare tu wo clouser 
 // kehlata he 
 
-function parent ()  {
+function parent() {
     let count = 0
     return function () {
         count++
         console.log(count);
     }
-   
-}
-const updateCount =
-parent()
-updateCount()
-updateCount()
 
+}
+const updateCount = parent()
+// updateCount()
+parent()()
+
+
+// useState
+
+function useState() {
+    var _count = "";
+    function updateCount(val) {
+        _count = val
+    }
+    return {
+        incrementVal() {
+            updateCount(+1)
+        },
+        decrementVal(val) {
+            updateCount(val)
+        },
+        returnValue() {
+            return _count
+        }
+    }
+
+}
+
+const forValueSet = useState()
+
+
+// forValueSet.setValue("hello World")
+forValueSet.incrementVal()
+forValueSet.decrementVal("hello")
+
+console.log("forInitialValue===",forValueSet.returnValue());
