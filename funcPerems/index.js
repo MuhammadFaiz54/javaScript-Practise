180a9f3c3e064990ef600a86dfef265e0ab1c7e0
Mac <mac@Macs-MacBook-Pro.local>
2024-04-17 18:42:09 +0500

    peremsLengthSet

---
+++ funcPerems/index.js
@@ -0,0 +1,18 @@
+const checkLength1 = function (a,b,c) {
+    
+}
+
+checkLength1.length
+// ouput = 3
+
+
+
+
+// isme jitna bhi perems daalo lo or phir iski length check karo 2 hi rahe gi 
+
+const checkLength2 = function (a,b,...c) {
+    
+}
+
+checkLength2.length
+// ouput = 2
